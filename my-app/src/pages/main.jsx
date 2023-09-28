e6227c646544e3ce04ecc70b934445e5303094ac
hemaddlawar <hema.200219627512@gmail.com>
2023-09-28 11:52:49 +0300

    done ;)

--- my-app/src/pages/main.jsx
+++ my-app/src/pages/main.jsx
@@ -2,18 +2,29 @@ import React from "react";
 import { useReducer } from "react";
 export default function Main() {
   function reducer(state, action) {
-    // ...
+    if (action.type === "addition") {
+      return {
+        ...state,
+        result: Number(state.firstNumber) + Number(state.secondNumber),
+      };
+    }
+    if (action.type === "subtraction") {
+      return {
+        ...state,
+        result: Number(state.firstNumber) - Number(state.secondNumber),
+      };
+    }
   }
 
   const [state, dispatch] = useReducer(reducer, {
     firstNumber: 0,
     secondNumber: 0,
+    result: 0,
   });
 
   ///
   const firstHandler = (event) => {
     state.firstNumber = event.target.value;
-    console.log(event.target.value);
   };
 
   /////
@@ -25,12 +36,11 @@ export default function Main() {
   //////////////
   return (
     <>
-      <form action="">
-        <label htmlFor=""> firstNumber</label>
-        <input type="number" onChange={firstHandler} />
-        <label htmlFor=""> second Number</label>
-        <input type="number" onChange={secondHandler} />
-      </form>
+      <label htmlFor=""> firstNumber</label>
+      <input type="number" onChange={firstHandler} />
+      <label htmlFor=""> second Number</label>
+      <input type="number" onChange={secondHandler} />
+
       <button
         onClick={() => {
           dispatch({ type: "addition" });
@@ -45,6 +55,7 @@ export default function Main() {
       >
         subtraction
       </button>
+      <div>{state.result}</div>
     </>
   );
 }
